5228e0cff6ec0555c433eeed3594f6d69b546c81
BartlomiejPoznanski <226104@student.uek.krakow.pl>
2023-06-14 16:05:50 +0200

    2023-06-14 BP

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,20 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, url_for
 
 @app.route('/main')
 def main():
     return render_template("main.html")
 
-@app.route('/extraction')
+@app.route('/extraction', methods=["POST",'GET'])
 def extraction():
+    if request.method=="POST":
+        product_code=request.form.get("product_code")
+        return redirect(url_for("product",product_code=product_code))
     return render_template("extraction.html")
 
-@app.route('/products_list')
-def products_list():
-    return render_template("product_list.html")
+@app.route('/products_list/<product_code>')
+def products_list(product_code):
+    return render_template("product_list.html", product_code=product_code)
 
 @app.route('/product_site')
 def product_site():
